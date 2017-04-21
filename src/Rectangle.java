07e3c498b2c057606c31cda333d4ccefba9ead3d
root <root@localhost.localdomain>
2017-04-21 07:11:04 -0700

    initial java commit

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle{
+	public int length;
+	public int width;
+	
+	public Rectangle(int length, int width){
+		this.length=length;
+		this.width=width;
+		}
+		
+		publicint getArea(){
+			return length*width;
+			}
+			
+		public int getPerimeter(){
+		return2*(length+width);
+		}
+	}
+	
